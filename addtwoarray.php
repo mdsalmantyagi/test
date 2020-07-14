35e518fa33c4ae5c81412d06c9b8a172522880d0
Salman <salmansallu1997@gmail.com>
2020-07-14 14:41:47 +0530

    first commit

--- addtwoarray.php
+++ addtwoarray.php
@@ -2,10 +2,9 @@
 
 $first = array(1, 2, 3, 4);
 $second = array(1, 1, 1, 1);
-
+$addTwoArray[]='';
 for($i = 0; $i < count($first); $i++){
-	echo $first[$i]+$second[$i];
-	echo "<br>";
+	$addTwoArray[]=$first[$i]+$second[$i];
 }
-
+echo implode(' ',$addTwoArray);
 ?>
--- arrayreverse.php
+++ arrayreverse.php
@@ -1,12 +1,9 @@
 <?php
 $array = array('1','2','3','4','5');
-
 $l = count($array);
-
+$arrayReverse[]='';
 for($i = ($l-1); $i >= 0; $i--){
-
-        echo $array[$i];
-		
+        $arrayReverse[]=$array[$i];
 		}
-
-	?>
+echo implode(' ',$arrayReverse);
+?>
